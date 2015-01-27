3898170d6f89036d7e506493c3e8d105c455a947
Jingo88 <jcory.ng@gmail.com>
2015-01-27 17:27:35 -0500

    finished Filter exercises

--- Filter/filter.js
+++ Filter/filter.js
@@ -5,4 +5,22 @@
 // The order of the non-zero numbers does not matter. -So [1, 0, 0, 1] becomes [0 ,0, 1, 1]. 
 // You may modify and return the given array or make a new array. For example:
 
+var zeroArr = [];
+var otherArr = [];
 
+function zeroFront(given){
+
+	for (i=0; i<given.length; i++){
+		if (given[i] === 0){
+			zeroArr.push(given[i]);
+		} else if (given[i] != 0){
+			otherArr.push(given[i]);
+		}
+	}
+
+	if (zeroArr.length > 0 && otherArr.length > 0){
+		return zeroArr.concat(otherArr)
+	}
+}
+
+console.log(zeroFront([1, 2, 6, 0, 0, 2, 0, 6, 0, 9]))
