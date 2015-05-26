0d32f4db83598d68c3451384a89786832b77f680
Jingo88 <jcory.ng@gmail.com>
2015-05-25 20:19:30 -0400

    finish switch

--- exercisesJS/vowels/README.md
+++ exercisesJS/vowels/README.md
@@ -8,10 +8,12 @@ For this exercise, consider "Y" to be a consonant.
 * For example, "Eat" returns "nope", "Ate" returns "It checks out"
 * Instead of logging "It checks out", log the consonants in the word.
 
-HINT: remember to put your argument in after running your javascript file. Your command line should look similar to this:
+HINT: Remember to put your argument in after running your javascript file. Your command line should look similar to this:
 
 ```
 	node vowels.js ate
 	// It checks out
 	// your consonants are t
 ```
+
+HINT 2: The answers are given in two different ways. After you are done completing the exercises take a look. Although the useSwitch file is longer than the useIfElse file it contains less characters. 
--- exercisesJS/vowels/useSwitch.js
+++ exercisesJS/vowels/useSwitch.js
@@ -22,24 +22,20 @@ for (i=0 ; i < check.length; i++){
 		default: consonants.push(letter);
 	};
 
-	// if ((letter === 'a') || (letter === 'e') || (letter === 'i') || (letter === 'o') || (letter ==='u')){
-	// 	count++;
-	// };
-
-	// if (((letter === 'a') || (letter === 'e') || (letter === 'i') || (letter === 'o') || (letter ==='u')) && ((nextLetter === 'a') || (nextLetter === 'e') || (nextLetter === 'i') || (nextLetter === 'o') || (nextLetter ==='u'))){
-	// 	adjacent++;
-	// };
-
-	// if ((arr[i] != 'a') && (arr[i] != 'e') && (arr[i] != 'i') && (arr[i] != 'o') && (arr[i] !='u')){
-	// 	consonants.push(arr[i]);
-	// }
+	switch(true){
+		case (letter==='a' || letter==='e' || letter==='i' || letter==='o' || letter==='u' )&& (nextLetter === 'a' || nextLetter==='e' || nextLetter==='i' || nextLetter==='o' || nextLetter==='u'):
+			adjacent++;
+			break;
+	}
 };
 
-if ((count > 1) && (adjacent < 1)){
-	console.log('It checks out');
-} else if ((count > 1) && (adjacent >= 1)){
-	console.log('nope');
-} else {
-	console.log('the vowels you seek are not here');
+switch(true){
+	case count > 1 && adjacent < 1: 
+		console.log('It checks out');
+		break;
+	case count >1 && adjacent >= 1:
+		console.log('nope');
+		break;
+	default: console.log('The vowels you seek are not here');
 }
 console.log('your consonants are ' + consonants);
